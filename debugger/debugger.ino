e6fdd08d2e15a574b89c8dc54db1bba495878949
PixelSergey <sergey.ichtchenko@gmail.com>
2020-07-25 14:10:17 +0300

    Add Arduino debugger to the project
    Simply hook up A0-A15 to even pins 22-52
    and D0-D7 to odd pins 31-45 on the Arduino Mega
    Also attach the CLK and RWB signals to the respective pins.
    Make sure to add a common GND connection between the devices.

---
+++ debugger/debugger.ino
@@ -0,0 +1,70 @@
+/**
+ * Arduino debugger for the 6502 chip.
+ * Credits to Ben Eater for the code structure (I have written the code myself basing it off the algorithm provided by Ben)
+ * 
+ *  Copyright (C) 2020 PixelSergey
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+int addressPins[16];
+int dataPins[8];
+
+#define CLK 3
+#define RW 4
+
+void pulse(){
+    unsigned int address = 0;
+    for(int i=0; i<=15; i++){  // Read address pins
+        int inbit = digitalRead(addressPins[i]) ? 1 : 0;
+        Serial.print(inbit);
+        address = (address << 1) + inbit;
+    }
+    Serial.print("    ");
+    
+    unsigned int data = 0;
+    for(int i=0; i<=7; i++){  // Read data pins
+        int inbit = digitalRead(dataPins[i]) ? 1 : 0;
+        Serial.print(inbit);
+        data = (data << 1) + inbit;
+    }
+
+    char converted[15];
+    sprintf(converted, "    %04x  %c  %02x", address, digitalRead(RW) ? 'r' : 'W', data);
+    Serial.print(converted);
+    Serial.println();
+}
+
+void setup() {
+    Serial.begin(57600);
+    
+    for(int i=0; i<=15; i++){  // Initialise address pins
+        addressPins[i] = 52 - 2*i; // Pins go 52, 50, ..., 22
+        pinMode(addressPins[i], INPUT);
+    }
+    
+    for(int i=0; i<=7; i++){  // Initialise data pins
+        dataPins[i] = 45 - 2*i;  // Pins go 45, 43, ..., 31
+        pinMode(dataPins[i], INPUT);
+    }
+    
+    pinMode(CLK, INPUT);
+    pinMode(RW, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(CLK), pulse, RISING);
+}
+
+void loop() {
+    
+}
