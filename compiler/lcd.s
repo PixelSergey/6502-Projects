4151040b718cd030a898900cce18b4c7b7cbb141
PixelSergey <sergey.ichtchenko@gmail.com>
2020-08-09 15:20:44 +0300

    Rewrite lcd.s to feature subroutines, loops
    Uses the RAM module (HM62256LP-70) for storing return addresses
    Waits for the LCD to be ready using the wait_for_lcd subroutine,
    and therefore supports higher clock speeds (1MHz at least)

--- compiler/lcd.s
+++ compiler/lcd.s
@@ -10,68 +10,75 @@ RS = %00100000
 
     .org $8000
 
+; Data section
+
+; Commands:
+; Function set: 8-bit mode, 2 lines, 5x8 font
+; Display control: display on, cursor on, blinking on
+; Entry mode: increment character position, do not shift screen
+; Clear display
+; Null byte
+to_send: .db %00111000,%00001111,%00000110,%00000001,0
+; String to send to the display
+to_print: .asciiz "Beep, boop! (2)"
+
+; Text section
 setup:
     ldx #$ff       ; Initialise stack pointer to FF
     txs
-
     lda #%11111111 ; Set all pins on port B to output
     sta DIRB
-
     lda #%11100000 ; Set relevant pins on port A to output
     sta DIRA
 
-    lda #%00111000 ; Function set: 8-bit mode, 2 lines, 5x8 font
+    ldx #0
+instr_loop:        ; Loop to send the instructions found in the data section
+    lda to_send,x
+    beq print_setup
     jsr send_instruction
+    inx
+    jmp instr_loop
 
-    lda #%00001111 ; Display control: display on, cursor on, blinking on
-    jsr send_instruction
-
-    lda #%00000110 ; Entry mode: increment character position, do not shift screen
-    jsr send_instruction
-
-    lda #%00000001 ; Clear display
-    jsr send_instruction
-
-    lda #"B"
+print_setup:
+    ldx #0
+print_loop:        ; Loop to send the data to the screen 
+    lda to_print,x
+    beq before_loop
     jsr send_data
+    inx
+    jmp print_loop
 
-    lda #"e"
-    jsr send_data
-
-    lda #"e"
-    jsr send_data
-
-    lda #"p"
-    jsr send_data
-
-    lda #","
-    jsr send_data
-
-    lda #" "
-    jsr send_data
-
-    lda #"b"
-    jsr send_data
-
-    lda #"o"
-    jsr send_data
-
-    lda #"o"
-    jsr send_data
-
-    lda #"p"
-    jsr send_data
-
-    lda #"!"
-    jsr send_data
-
-    lda #%00110000 ; Set DDRAM address to start of next line 
+before_loop:
+    lda #%11000000 ; Set the DDRAM address to line 2
     jsr send_instruction
 
 loop:
     jmp loop
 
+wait_for_lcd:
+    pha
+    lda #%00000000 ; Set all pins on port B to input
+    sta DIRB
+read_flag:
+    lda #RWB       ; Enable RWB; clear RS, EN
+    sta PORTA
+    lda #(RWB | EN); Enable EN, RWB bit (send instruction)
+    sta PORTA
+
+    lda PORTB      ; Read busy flag
+    and #%10000000 ; Isolate busy flag
+    bne read_flag  ; Jump back if result was not 0 (busy flag is 1)
+
+    lda #%11111111 ; Set all pins on port B to output
+    sta DIRB
+    lda #RWB
+    sta PORTA
+
+    pla
+    rts
+
 send_instruction:
+    jsr wait_for_lcd
     sta PORTB
     lda #0         ; Clear RS, RWB, EN bits
     sta PORTA
@@ -82,6 +89,7 @@ send_instruction:
     rts
 
 send_data:
+    jsr wait_for_lcd
     sta PORTB
     lda #RS         ; Set RS; clear RWB, EN bits
     sta PORTA
